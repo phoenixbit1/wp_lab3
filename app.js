d3ddfe68a058519b49bf028ce6871fddc7c15798
phoenixbit1 <sn2002@hw.ac.uk>
2021-11-21 21:06:52 +0400

    commit

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index"); //no need for ejs extension
 });
+//pass requests to the router middleware
+const router = require('./routes/apis');
+app.use(router);
 app.get("/contacts", (req, res) => {
   res.render("contacts");
 });
@@ -19,3 +22,6 @@ const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
 });
+// using JSON and URL Encoded middleware 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
---
+++ controllers/productController.js
@@ -0,0 +1,30 @@
+const getCatalogue = (request, response) => {
+    const catalogServices = require('../services/productServices');
+    catalogServices.searchService(function(err, rows) {
+        response.json(rows);
+        response.end();
+    });
+};
+
+const getProductByID = (request, response) => {
+    const catalogServices = require('../services/productServices');
+    let reference = request.params.reference;
+    catalogServices.searchIDService(reference, function(err, rows) {
+        response.json(rows);
+        response.end();
+    });
+};
+
+const getProductsByCategory = (request, response) => {
+    const catalogServices = require('../services/productServices');
+    let reference = request.params.category;
+    catalogServices.searchCategoryService(category, function(err, rows) {
+        response.render('catalog', { products: rows });
+    });
+};
+
+module.exports = {
+    getCatalogue,
+    getProductByID,
+    getProductsByCategory
+};
